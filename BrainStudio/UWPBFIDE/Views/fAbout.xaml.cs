d99ea99d75f85d5db7288242f7512965e0e9ec7a
@Mohsens22 <mohsens2@outlook.com>
2017-02-10 03:03:34 +0330

    -Brain Studio went to BETA phase -Fixed about page -small fixes

--- BrainStudio/UWPBFIDE/Views/fAbout.xaml.cs
+++ BrainStudio/UWPBFIDE/Views/fAbout.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +24,13 @@ namespace UWPBFIDE.Views
     /// </summary>
     public sealed partial class fAbout : Page
     {
+        private DataTransferManager dataTransferManager;
         public fAbout()
         {
             this.InitializeComponent();
+            ApplicationName.Text = Package.Current.DisplayName;
+            var v = Package.Current.Id.Version;
+            ApplicationVersion.Text = string.Format("{0}.{1}.{2}.{3}", v.Major, v.Minor, v.Build, v.Revision);
         }
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
@@ -37,6 +43,57 @@ namespace UWPBFIDE.Views
             {
                 MainPage.Current.Back.Visibility = Visibility.Collapsed;
             }
+
+            // register this page as share source
+            this.dataTransferManager = DataTransferManager.GetForCurrentView();
+            this.dataTransferManager.DataRequested += new TypedEventHandler<DataTransferManager, DataRequestedEventArgs>(this.DataRequested);
+        }
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            // unregister as share source
+            this.dataTransferManager.DataRequested -= new TypedEventHandler<DataTransferManager, DataRequestedEventArgs>(this.DataRequested);
+        }
+        private void DataRequested(DataTransferManager sender, DataRequestedEventArgs e)
+        {
+            Uri dataPackageUri = new Uri("https://www.microsoft.com/store/apps/9nk2p1vzmq23");
+            DataPackage requestData = e.Request.Data;
+            requestData.Properties.Title = "Brain Studio | Code everywhere!";
+            requestData.SetWebLink(dataPackageUri);
+            requestData.Properties.Description = "Check out the UWP IDE for BrainFuck Programming Language";
+        }
+        private async void RateApp(object sender, RoutedEventArgs e)
+        {
+            await Windows.System.Launcher.LaunchUriAsync(new Uri(string.Format("ms-windows-store:REVIEW?PFN={0}", Windows.ApplicationModel.Package.Current.Id.FamilyName)));
+        }
+
+        private async void TelegramChannel(object sender, RoutedEventArgs e)
+        {
+            await Windows.System.Launcher.LaunchUriAsync(new Uri("http://t.me/irmahgraphic", UriKind.RelativeOrAbsolute));
+        }
+        private async void InstaPage(object sender, RoutedEventArgs e)
+        {
+            await Windows.System.Launcher.LaunchUriAsync(new Uri("https://www.instagram.com/mah_graphic_studio", UriKind.RelativeOrAbsolute));
+        }
+        private void share_Click(object sender, RoutedEventArgs e)
+        {
+            Windows.ApplicationModel.DataTransfer.DataTransferManager.ShowShareUI();
+        }
+        private async void rate_Click(object sender, RoutedEventArgs e)
+        {
+            await Windows.System.Launcher.LaunchUriAsync(new Uri(string.Format("ms-windows-store:REVIEW?PFN={0}", Windows.ApplicationModel.Package.Current.Id.FamilyName)));
+        }
+        private async void fback_Click(object sender, RoutedEventArgs e)
+        {
+            if (Microsoft.Services.Store.Engagement.StoreServicesFeedbackLauncher.IsSupported())
+            {
+                var launcher = Microsoft.Services.Store.Engagement.StoreServicesFeedbackLauncher.GetDefault();
+                await launcher.LaunchAsync();
+            }
+            else
+            {
+                await Windows.System.Launcher.LaunchUriAsync(new Uri("mailto:ngame1390@live.com?subject=WikiSeda_U3_FeedBack&cc=mohsens22@outlook.com"));
+            }
+
         }
     }
 }
