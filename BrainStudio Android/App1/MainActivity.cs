3431c143948a275868f42b1893c6239b6c9b7e3a
@Mohsens22 <mohsens2@outlook.com>
2017-03-06 16:17:21 +0330

    BS4A now can compile BrainFuck codes

--- BrainStudio Android/App1/MainActivity.cs
+++ BrainStudio Android/App1/MainActivity.cs
@@ -13,7 +13,16 @@ namespace BFIDE
     public class MainActivity : Activity
     {
         int count = 1;
+        private static bool isopen = false;
+        private static string VER = "0.0.0.1";
 
+        private static readonly int BUFSIZE = 65535;
+
+        private int[] buf = new int[BUFSIZE];
+
+        private int ptr { get; set; }
+
+        private bool echo { get; set; }
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -23,7 +32,208 @@ namespace BFIDE
 
             // Get our button from the layout resource,
             // and attach an event to it
+            var editText = FindViewById<EditText>(Resource.Id.editText);
             
+                
+            var button = FindViewById<Button>(Resource.Id.button2);
+            button.Click += delegate
+            {
+                Interpret(">"+editText.Text);
+                
+            };
+
+        }
+        public async void Interpret(string s)
+
+        {
+            try
+            {
+                int i = 0;
+
+                int right = s.Length;
+
+                while (i < right)
+
+                {
+
+                    switch (s[i])
+
+                    {
+
+                        case '>':
+
+                            {
+
+                                this.ptr++;
+
+                                if (this.ptr >= BUFSIZE)
+
+                                {
+
+                                    this.ptr = 0;
+
+                                }
+
+                                break;
+
+                            }
+
+                        case '<':
+
+                            {
+
+                                this.ptr--;
+
+                                if (this.ptr < 0)
+
+                                {
+
+                                    this.ptr = BUFSIZE - 1;
+
+                                }
+
+                                break;
+
+                            }
+
+                        case '.':
+
+                            {
+                                var outp = FindViewById<TextView>(Resource.Id.textView);
+                                outp.Text += ((char)this.buf[this.ptr]);
+
+                                break;
+
+                            }
+
+                        case '+':
+
+                            {
+
+                                this.buf[this.ptr]++;
+
+                                break;
+
+                            }
+
+                        case '-':
+
+                            {
+
+                                this.buf[this.ptr]--;
+
+                                break;
+
+                            }
+
+                        case '[':
+
+                            {
+
+                                if (this.buf[this.ptr] == 0)
+
+                                {
+
+                                    int loop = 1;
+
+                                    while (loop > 0)
+
+                                    {
+
+                                        i++;
+
+                                        char c = s[i];
+
+                                        if (c == '[')
+
+                                        {
+
+                                            loop++;
+
+                                        }
+
+                                        else
+
+                                        if (c == ']')
+
+                                        {
+
+                                            loop--;
+
+                                        }
+
+                                    }
+
+                                }
+
+                                break;
+
+                            }
+
+                        case ']':
+
+                            {
+
+                                int loop = 1;
+
+                                while (loop > 0)
+
+                                {
+
+                                    i--;
+
+                                    char c = s[i];
+
+                                    if (c == '[')
+
+                                    {
+
+                                        loop--;
+
+                                    }
+
+                                    else
+
+                                    if (c == ']')
+
+                                    {
+
+                                        loop++;
+
+                                    }
+
+                                }
+
+                                i--;
+
+                                break;
+
+                            }
+
+                        case ',':
+
+                            {
+
+                                // read a key
+                                var inp = FindViewById<EditText>(Resource.Id.inpText);
+                                string key = inp.Text;
+                                this.buf[this.ptr] = (int)Convert.ToChar(key);
+
+                                break;
+
+                            }
+
+                    }
+
+                    i++;
+
+                }
+            }
+            catch
+            {
+                
+            }
+
         }
     }
 }
--- BrainStudio Android/App1/Resources/Resource.Designer.cs
+++ BrainStudio Android/App1/Resources/Resource.Designer.cs
@@ -57,6 +57,31 @@ namespace BFIDE
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050003
+			public const int button2 = 2131034115;
+			
+			// aapt resource value: 0x7f050000
+			public const int editText = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int inpText = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int textView = 2131034114;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
